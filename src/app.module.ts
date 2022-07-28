d90891cf190e3b436bd1ef40021cf9b61f1774e0
MrBrown6210 <bite6210@gmail.com>
2022-07-28 23:01:37 +0700

    Connect to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeormConfig } from './infrastructure/configs/ormconfig';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [TypeOrmModule.forRoot(typeormConfig), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,21 +1,25 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CreateUserHandler } from './commands/create-user/create-user.handler';
 import { CreateUserHttpController } from './commands/create-user/create-user.http.controller';
+import { UserOrmEntity } from './database/user.orm-entity';
+import { UserOrmMapper } from './database/user.orm-mapper';
+import { UserRepository } from './database/user.repository';
 import { UserProvider } from './user.provider';
 
 const httpControllers = [CreateUserHttpController];
 
 const messageController = [];
 
-const repositories = [];
+const repositories = [UserRepository, UserOrmMapper];
 
 const commandHandlers = [CreateUserHandler];
 
 const queryHandlers = [];
 
 @Module({
-  imports: [CqrsModule],
+  imports: [TypeOrmModule.forFeature([UserOrmEntity]), CqrsModule],
   controllers: [...httpControllers],
   providers: [
     ...messageController,
